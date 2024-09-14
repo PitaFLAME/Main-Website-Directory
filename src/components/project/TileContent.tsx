d8ee81c571eccf33ec680ff6e426ace72064161a
Pita <pita.sherwood@protonmail.com>
2024-09-13 19:39:32 -0500

    Progress on Projects

--- src/components/project/TileContent.tsx
+++ src/components/project/TileContent.tsx
@@ -5,20 +5,20 @@ import { getSvg } from '../SvgData'
 
 
 const content = [
-    (             <div key={0} className="mt-2">
-            {getSvg(3, 9, "w-96 -ml-12 mt-8")}
+    (             <div key={0} className="relative w-full h-full mt-2">
+            {getSvg(3, 9, "w=[110%] mt-8")}
 
 
-        </div>), (<div key={1} className="" >
-            {getSvg(5, 7, "p-8 h-[100%]")}
+        </div>), (<div key={1} className="flex justify-center h-full w-full" >
+            {getSvg(5, 6, "h-[100%] p-4")}
 
 
-        </div>), (<div key={2} className="mt-2" >
-            {getSvg(4, 7, "h-72 ml-auto mr-8")}
+        </div>), (<div key={2} className="h-full" >
+            {getSvg(4, 6, "h-[100%] py-3 ml-auto mr-8")}
 
 
-        </div>), (<div key={3}>
-            {getSvg(6, 9, "h-72 mt-2 ml-auto mr-12")}
+        </div>), (<div key={3} className="h-full" >
+            {getSvg(6, 9, "h-[100%] py-2 ml-auto mr-12")}
 
 
         </div>)
