84ca2e72a0116b2022713b3b49e28fa7806cad96
Pita <pita.sherwood@protonmail.com>
2024-09-17 07:05:59 -0500

    Added keys to array elements

--- src/app/cooldowns/TileContent.tsx
+++ src/app/cooldowns/TileContent.tsx
@@ -3,12 +3,12 @@ import Link from "next/link"
 
 
 const content = [
-    (<Link className="flex items-center h-full w-full group" href="/" >
+    (<Link key={0} className="flex items-center h-full w-full group" href="/" >
         <ChevronLeft className="ml-auto mr-6 h-20 w-20 opacity-0 group-hover:opacity-100 duration-200" />
     </Link>), (
-        <h2 className="flex justify-center items-center w-full h-full text-slate-950 font-poppins text-2xl cursor-default" >Features</h2>
+        <h2 key={1} className="flex justify-center items-center w-full h-full text-slate-950 font-poppins text-2xl cursor-default" >Features</h2>
     ), (
-        <h2 className="flex justify-center items-center w-full h-full text-slate-950 font-poppins text-2xl cursor-default" >Future Updates</h2>
+        <h2 key={2} className="flex justify-center items-center w-full h-full text-slate-950 font-poppins text-2xl cursor-default" >Future Updates</h2>
     )
 ]
 
