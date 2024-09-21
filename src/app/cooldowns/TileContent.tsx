b4a434afcc4976881897bc393a628d0f973e8d68
Pita <pita.sherwood@protonmail.com>
2024-09-20 23:36:00 -0500

    fixing responsiveness of /cooldowns

--- src/app/cooldowns/TileContent.tsx
+++ src/app/cooldowns/TileContent.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 
 const content = [
     (<Link key={0} className="flex items-center h-full w-full group" href="/" >
-        <ChevronLeft className="ml-auto mr-6 h-20 w-20 opacity-0 group-hover:opacity-100 duration-200" />
+        <ChevronLeft className="ml-auto text-white mr-6 h-20 w-20 opacity-0 group-hover:opacity-100 duration-200" />
     </Link>), (
         <h2 key={1} className="flex justify-center items-center w-full h-full text-slate-950 font-poppins text-2xl cursor-default" >Features</h2>
     ), (
--- src/app/cooldowns/page.tsx
+++ src/app/cooldowns/page.tsx
@@ -8,7 +8,8 @@ const Home = () => {
 
 
     return (
-        <main className="h-full w-full bg-gradient-to-b from-slate-600 to-slate-700 overflow-y-scroll scroll-smooth">
+        <main className="h-full w-full bg-gradient-to-b from-slate-600 to-slate-700 overflow-y-scroll scroll-smooth
+                       text-slate-950 text-md lg:text-xl">
 
             <div className="fixed mt-12 w-64 h-48 -ml-32 z-20" >
                 <Tile accentID={9} orientation={2} render={getContent(0)} />
@@ -18,9 +19,9 @@ const Home = () => {
                 
                 {getSvg(3, 1, "absolute right-0 w-[40vw] h-[40vh] max-h-[512px] max-w-[512px] mr-24 mt-8")}
 
-                <div className="grid grid-cols-12 grid-rows-12
+                <div className="grid grid-cols-12 grid-rows-12 
                     mt-24 px-8
-                    h-[70vh] w-full gap-2 bg-slate-100 opacity-90">
+                    h-[820px] w-full gap-2 bg-slate-100 opacity-90">
 
                     
                     <div className="row-span-2" />
@@ -28,7 +29,7 @@ const Home = () => {
                         <h1 className="text-zinc-950 font-poppins text-4xl" >Cooldowns App</h1>
                     </div>
 
-                    <div className="text-slate-950 text-xl row-span-3 col-span-8">
+                    <div className="row-span-3 col-span-8">
                         <p className="indent-12">
                         Welcome to my Cooldowns App project page. This Lifestyle app for mobile 
                         platforms was developed using Dart and is designed to help manage impulsivity 
@@ -37,7 +38,7 @@ const Home = () => {
                         or study, this life-gamifying application seeks to aid you.
                         </p>
                     </div><div className="row-span-3 col-span-4" />
-                    <div className="text-slate-950 text-xl row-span-3 col-span-10">
+                    <div className="row-span-3 col-span-10">
                         <p className="indent-12">
                         In its current state, the app allows you to choose from a short list of icons to 
                         represent your Cooldown, and customize the recharge time, number of charges, and the 
@@ -50,7 +51,7 @@ const Home = () => {
                     <div className="col-span-12" />
                     <Tile accentID={3} sizeType={2} orientation={2} render={getContent(1)} />
                     <div className="col-span-10" />
-                    <div className="flex items-end text-slate-950 text-xl row-span-2 col-span-11">
+                    <div className="flex items-end row-span-2 col-span-11">
                         <p className="indent-12">
                             The app will be empty when you first download it, so the first thing you should do is create 
                             a new Cooldown. Creating new Cooldowns is fairly straightforward. You can specify the name of 
@@ -65,7 +66,7 @@ const Home = () => {
                     px-8 z-10
                     h-[70vh] w-full gap-2 bg-slate-100 opacity-90">
 
-                    <div className="flex items-center text-slate-950 text-xl row-span-3 col-span-7">
+                    <div className="flex items-center row-span-3 col-span-7">
                         <p className="indent-12">
                         You can also expand the &quot;Advanced&quot; menu to specify a number of charges the cooldown has, and how 
                         those charges recharge. For Parallel, each expended charge of the Cooldown will recharge independently 
@@ -91,7 +92,7 @@ const Home = () => {
                         />
                     </div>
                     <div className="row-span-3 col-span-3" />
-                    <div className="flex items-end row-span-3 col-span-7 text-slate-950 text-xl pl-2">
+                    <div className="flex items-end row-span-3 col-span-7 pl-2">
                         <p className="indent-12">
                         The main Cooldowns view page displays all of the Cooldowns you&apos;ve created. The icon will appear 
                         normal if it is available. If the Cooldown has multiple charges available, it will have a small 
@@ -111,11 +112,11 @@ const Home = () => {
                     <Tile accentID={3} sizeType={12} orientation={2} render={getContent(2)} />
                     <div className="col-span-9" />
                     <div className="row-span-4" />
-                    <div className="col-span-8 row-span-4 text-slate-950 text-xl">
+                    <div className="col-span-8 row-span-4">
                         <p className="mt-2 mb-4">
                         Going forward, the following features are planned to be added:
                         </p>
-                        <ul className="list-disc text-xl ml-8">
+                        <ul className="list-disc ml-8">
                             <li>
                             Custom Icons. Users will be able to upload photos from their device to create icons for their cooldowns.
                             </li>
