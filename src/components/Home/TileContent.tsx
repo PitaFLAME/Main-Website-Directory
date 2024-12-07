10d910e02b1399fa808bf85bd00a1a826f2884c8
Pita <pita.sherwood@protonmail.com>
2024-12-06 19:38:38 -0600

    Updated quote, quote image.  Removed content from being available immediately.  Added placeholder icons

--- src/components/Home/TileContent.tsx
+++ src/components/Home/TileContent.tsx
@@ -2,11 +2,9 @@
 
 
 import StackIcon from "tech-stack-icons"
-import Image from "next/image"
 import { getSvg } from "../SvgData";
 import { getAccent } from "../Globals";
-import { get } from "http";
-import { Quote } from "lucide-react";
+import { ChevronRight, Circle, Quote } from "lucide-react";
 import React from "react";
 
 const icons = [
@@ -23,7 +21,7 @@ const getIcons = () => {
                                         <div className={`absolute opacity-60 group-hover:opacity-10 h-full w-full top-0
                                             ${getAccent(8, 'bg')}`} />
                                     </div>
-     )); 
+     ));
 }
 
 
@@ -33,15 +31,16 @@ const content = [
                 Hi, my name is&nbsp;
                 <span className="text-7xl ml-2 font-allison text-slate-200">Pita</span>
             </h2>
-            {/*<div className="flex flex-row w-full h-full px-8 gap-2">
-                <p className="text-right w-full text-lg font-poppins">I do a lot of <span>things</span></p>
-                <div className="flex justify-center w-full">
-                    <ThingsCarousel />
-                </div> 
-            </div>*/}
+            
 
-        </div> ), ( <div key={1} className="relative flex flex-row gap-4 p-6 h-full w-full" >
-            <div className={`h-full w-[1px] ml-2 ${getAccent(7, 'bg')}`} />
+        </div> ), ( <div key={1} className="relative flex flex-row items-center group gap-4 p-6 h-full w-full" >
+            <div className="relative flex items-center h-full w-56 ml-auto">
+                <Circle className={`h-56 w-56 ${getAccent(8, 'text')}`} />
+                {getSvg(7, 8, "absolute w-44 h-44 left-6 bottom-10")}
+            </div>
+            <ChevronRight className={`w-24 h-24 -ml-6 ${getAccent(8, 'text')} ${getAccent(6, 'group-hover:text')}`} />
+
+            {/*<div className={`h-full w-[1px] ml-2 ${getAccent(7, 'bg')}`} />
             <div className="flex flex-col w-full gap-2">
                 <h2 className="text-3xl mb-2 font-extralight">Education</h2>
                 <div className="flex flex-row w-full">
@@ -58,20 +57,27 @@ const content = [
                     </div>
                     <p className={`ml-auto text-base ${getAccent(7, 'text')}`}>August 2016 - January 2019</p>
                 </div>
-            </div>
+            </div>*/}
 
-        </div> ), ( <div key={2} className="flex flex-col px-8 pt-4 items-center">
-            <h2 className={`text-2xl mb-2.5 ml-2 font-poppins w-full ${getAccent(2, 'text')}`}>
-                Technologies I use</h2>
-            <div className="relative grid grid-cols-4 gap-6 mt-3 max-w-68">
-                {getIcons()}
+        </div> ), ( <div key={2} className="flex flex-col px-8 py-16 justify-end items-center h-full w-full">
+            {/*<h2 className={`text-2xl mb-2.5 ml-2 font-poppins w-full ${getAccent(2, 'text')}`}>
+                My Stack
+            </h2>*/}
+
+            <div className="grid columns-1">
+                <Circle className={`h-56 w-56 ${getAccent(7, 'text')} row-start-1 col-start-1`} />
+                {getSvg(7, 7, "w-44 h-44 row-start-1 col-start-1 ml-6 mt-6")}
             </div>
+            
+            {/*<div className="relative grid grid-cols-4 gap-6 mt-3 max-w-68">
+                {getIcons()}
+            </div>*/}
             
         </div> ), ( <div key={3} className="relative flex flex-col justify-center items-center h-full gap-4 group">
             <div className="absolute w-24 -top-4 right-4">
                 {getSvg(0, 8)}
             </div>
-
+            
             <img src="https://avatars.githubusercontent.com/u/171903351?v=4"
                    alt="GitHub Profile"
                    className={`rounded-full w-48 h-48 border-8 ${getAccent(8, 'border')}
@@ -83,9 +89,12 @@ const content = [
             <div className="absolute z-10 mt-12 w-[90%]">
                 <Quote className="w-10 h-10 fill-sky-500 text-sky-400" />
                 <h2 className="text-center text-3xl font-semibold mt-4 leading-normal font-poppins" >
-                    I will not die as I am.  I will live long enough to become somebody else.
+                    Sometimes, through the noise of its thoughts, it hears the universe.
                 </h2>
                 <Quote className="w-10 h-10 fill-sky-500 text-sky-400 ml-auto mt-2" />
+                <h3 className="text-white font-poppins text-right mt-4 mr-4">
+                    - Julian Gough
+                </h3>
             </div>
 
         </div> ), ( <div key={5} className="relative w-full h-full">
@@ -94,7 +103,6 @@ const content = [
         </div> ), ( <div key={6} className="w-full h-full" >
             <h2>Full </h2>
         </div> )
-
 ]
 
 
